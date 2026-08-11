f3d05df8dfd33d7c9b5ef35d8eedcc43408b14d8
dinizmarceloferreira-jpg <dinizmarceloferreira@gmail.com>
2026-08-11 09:07:41 -0300

    Add files via upload

--- src/src/lib/analytics.ts
+++ src/src/lib/analytics.ts
@@ -3,7 +3,7 @@ export function trackWhatsAppClick(location: string) {
     if ('gtag' in window) {
       // @ts-ignore
       window.gtag('event', 'conversion', {
-        send_to: 'AW-18226966667/-x5OCMSr6d8cEIvhpfND',
+        send_to: 'AW-18226966667/tFe3COiH7d8cEIvhpfND',
         event_category: 'engagement',
         event_label: location,
       });
