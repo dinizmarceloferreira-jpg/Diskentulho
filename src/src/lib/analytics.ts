804e3de73d728062a02b4a3ced3b0f702bb06cc9
dinizmarceloferreira-jpg <dinizmarceloferreira@gmail.com>
2026-08-11 08:57:43 -0300

    Add files via upload

--- src/src/lib/analytics.ts
+++ src/src/lib/analytics.ts
@@ -1,10 +1,17 @@
 export function trackWhatsAppClick(location: string) {
-  if (typeof window !== 'undefined' && 'gtag' in window) {
+  if (typeof window !== 'undefined') {
+    if ('gtag' in window) {
+      // @ts-ignore
+      window.gtag('event', 'conversion', {
+        send_to: 'AW-18226966667/-x5OCMSr6d8cEIvhpfND',
+        event_category: 'engagement',
+        event_label: location,
+      });
+    }
     // @ts-ignore
-    window.gtag('event', 'conversion', {
-      send_to: 'AW-18226966667/-x5OCMSr6d8cEIvhpfND',
-      event_category: 'engagement',
-      event_label: location,
-    });
+    if (typeof window.gtag_report_conversion === 'function') {
+      // @ts-ignore
+      window.gtag_report_conversion();
+    }
   }
 }
